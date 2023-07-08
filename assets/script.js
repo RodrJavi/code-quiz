c4fc3e280df1709d3a1dc8ab0551e335886fa4f5
Javi <nb2javi@gmail.com>
2023-07-08 16:35:45 -0500

    More dynamic question loading changes and switch statement

--- assets/script.js
+++ assets/script.js
@@ -7,12 +7,12 @@ function startQuiz() {
   let quizQuestion = document.querySelector("#quiz-question");
   let answerList = document.querySelector("#answer-list").children;
   let chosenAnswer;
-  let firstQuestion = {
+  let question1 = {
     Question: "This is the question in firstQuestion",
     answers: ["First answer", "Second answer", "Third answer", "Fourth answer"],
     values: [false, true, false, false],
   };
-  let secondQuestion = {
+  let question2 = {
     Question: "This is the question in secondQuestion",
     answers: [
       "First answer(2)",
@@ -22,6 +22,57 @@ function startQuiz() {
     ],
     values: [false, false, false, true],
   };
+  let question3 = {
+    Question: "This is the question in thirdQuestion",
+    answers: [
+      "First answer(3)",
+      "Second answer(3)",
+      "Third answer(3)",
+      "Fourth answer(3)",
+    ],
+    values: [true, false, false, false],
+  };
+
+  // Changes HTML to show current questions and adds event listeners to answers that provid a true or false boolean
+  function renderQuestion(currentQuestion) {
+    quizQuestion.textContent = currentQuestion.Question;
+    for (let i = 0; i < currentQuestion.answers.length; i++) {
+      answerList[i].textContent = currentQuestion.answers[i];
+      answerList[i].addEventListener("click", () => {
+        chosenAnswer = currentQuestion.values[i];
+        result(chosenAnswer, currentQuestion);
+      });
+    }
+  }
+
+  // Takes clicked answer from active question
+  function result(response, currentQuestion) {
+    if (response) {
+      console.log("No penalty!");
+    } else {
+      console.log("You're wrong! Minus 10 seconds!");
+      timerCount = timerCount - 10;
+    }
+
+    switch (currentQuestion) {
+      case question1:
+        renderQuestion(question2);
+        break;
+      case question2:
+        renderQuestion(question3);
+        break;
+      case question3:
+        renderQuestion(question4);
+        break;
+      case question4:
+        renderQuestion(question5);
+        break;
+      case question5:
+        renderQuestion(question6);
+        break;
+    }
+    document.querySelector("#result").textContent = response;
+  }
 
   screenTimer.textContent = "Time: " + timerCount;
 
@@ -40,33 +91,7 @@ function startQuiz() {
   // document.querySelector(".quiz-intro").style.display = "none";
   // document.querySelector(".active-quiz").style.display = "block";
 
-  // Changes HTML to show current questions and adds event listeners to answers that provid a true or false boolean
-  function renderQuestion(currentQuestion) {
-    quizQuestion.textContent = currentQuestion.Question;
-    for (let i = 0; i < currentQuestion.answers.length; i++) {
-      answerList[i].textContent = currentQuestion.answers[i];
-      answerList[i].addEventListener("click", () => {
-        console.log("Click on " + i);
-        chosenAnswer = currentQuestion.values[i];
-        console.log(chosenAnswer);
-        result(chosenAnswer);
-      });
-    }
-  }
-
-  renderQuestion(secondQuestion);
-
-  function result(response) {
-    if (response) {
-      console.log("No penalty!");
-    } else {
-      console.log("You're wrong! Minus 10 seconds!");
-      timerCount = timerCount - 10;
-    }
-    document.querySelector("#result").textContent = response;
-  }
-
-  console.log(firstQuestion);
+  renderQuestion(question1);
 }
 
 quizStartButton.addEventListener("click", startQuiz);
